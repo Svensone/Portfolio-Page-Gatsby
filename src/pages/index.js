cd628b24b0c5f105412eb1543353231127f41d38
svenson.e <svenson.ertel@gmail.com>
2021-01-22 11:31:47 +0800

    cleaning layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -107,16 +107,16 @@ class HomeIndex extends React.Component {
                             link
                         }))} />
 
-                        <ul className="actions">
+                        {/* <ul className="actions">
                             <li><a href="#" className="button">Full Portfolio</a></li>
-                        </ul>
+                        </ul> */}
                     </section>
 
                     <section id="three">
                         <h2>Get In Touch</h2>
-                        <p>to stay connected & so you can enjoy my future works as well</p>
+                        {/* <p>to stay connected & so you can enjoy my future works as well</p> */}
                         <div className="row">
-                            <div className="8u 12u$(small)">
+                            {/* <div className="8u 12u$(small)">
                                 <form method="post" action="#">
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
@@ -127,22 +127,24 @@ class HomeIndex extends React.Component {
                                 <ul className="actions">
                                     <li><input type="submit" value="Send Message" /></li>
                                 </ul>
-                            </div>
+                            </div> */}
                             <div className="4u 12u$(small)">
                                 <ul className="labeled-icons">
                                     <li>
                                         <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        Beach street 101 <br />
-                                        Bali <br />
-                                        Indonesia
+                                        82261 Bali <br />
+                                        Indonesia <br />
+                                        / <br />
+                                        80679 Munich <br />
+                                        Germany
                                     </li>
                                     <li>
                                         <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
+                                        -
                                     </li>
                                     <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">hello@untitled.tld</a>
+                                        <a href="#">svenson.ertel@gmail.com</a>
                                     </li>
                                 </ul>
                             </div>
