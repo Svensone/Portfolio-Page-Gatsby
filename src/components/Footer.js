b2ee43191dbf5027c2114f8490b0ee57505f76d2
svenson.e <svenson.ertel@gmail.com>
2021-03-15 16:33:15 +0800

    email link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="https://twitter.com/ErtelSven" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                         <li><a href="https://github.com/Svensone?tab=repositories" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        {/* <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li> */}
+                        <li><a href="mailto:svenson.ertel@gmail.com" target="_blank" className="icon fa-envelope-o"><span className="label" >Email</span></a></li>
                     </ul>
                     
                 </div>
