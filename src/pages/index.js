31d7fb337b08cc6c89a1e0e7d212fbe669328907
svenson.e <svenson.ertel@gmail.com>
2021-03-27 13:18:34 +0800

    adjusting links of project

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ import full11 from '../assets/images/fulls/103.jpg'
 
 const DEFAULT_IMAGES = [
     { id: '1', src: full01, thumbnail: thumb01, caption: 'Bali Covid Dash App', description: 'Dash App deployed on Heroku : Displaying Regency Data for Bali - made with Python, Plotly, Scrapy', link: 'https://bali-covid-app.herokuapp.com/'},
-    { id: '2', src: full07, thumbnail: thumb07, caption: 'fastAI/pytorch ML Image Recognition', description: 'ResNet34 CNN trained on balinese dance-style images, accuracy 86% - made with fastai, pytorch, Python', link: 'http://b3eb0f5587b2.ngrok.io/' },
+    { id: '2', src: full07, thumbnail: thumb07, caption: 'fastAI/pytorch ML Image Recognition', description: 'ResNet34 CNN trained on balinese dance-style images, accuracy 86% - made with fastai, pytorch, Python', link: 'https://share.streamlit.io/svensone/fastai2-bali-dance/main/app.py' },
     { id: '8', src: full08, thumbnail: thumb08, caption: 'Medical Pneumonia detection',     description: 'Image Recognition on lung x-rays to detect pneumonia', link: 'https://share.streamlit.io/svensone/-fastai2-pneumonia-cv/app.py'},
     { id: '9', src: full09, thumbnail: thumb09, caption: 'Dataset Bali Regency Covid Data', description: 'Dataset creation by scrpy data with python scrapy, processing with Pandas', link: 'https://www.kaggle.com/svensone/covid19-bali-regencies-data'},
     { id: '10', src: full10, thumbnail: thumb10, caption: 'Garmin Watch Widget', description: 'Garmin Slope Detection App (MonkeyC', link: 'https://github.com/Svensone/Garmin-Slope-App'},
