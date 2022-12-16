917069abd8ae19540fb8477b7a4864cecb8a5f79
svensone <svenson.ertel@gmail.com>
2022-12-17 07:58:18 +0800

    deleted link to netlify page

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const DEFAULT_IMAGES = [
     { id: '6', src: full05, thumbnail: thumb05, caption: 'Simple Classic Html Homepage', 
     description: 'made with Bootstrap, Html, Sass & some JS Animations', link: 'https://svensone.github.io/'},
     { id: '7', src: full06, thumbnail: thumb06, caption: 'Welcome to Bali - the Project', 
-    description: 'HTML / CSS / JS Site with anime.js and other animations', link: 'https://svenson-bali-homepage.netlify.com/'},
+    description: 'HTML / CSS / JS Site with anime.js and other animations', link: ''},
 ];
 
 class HomeIndex extends React.Component {
